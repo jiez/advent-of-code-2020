3fea78e380eeda6ef183e367249f9f664760b18a
Jie Zhang <jzhang918@gmail.com>
2020-12-11 01:09:44 -0500

    solution for puzzle 2 of day 11

--- 11/seating.cc
+++ 11/seating.cc
@@ -6,6 +6,14 @@
 #include <vector>
 #include <algorithm>
 
+
+static void print_seats(const std::vector<std::string>& seats)
+{
+    for (auto str: seats)
+        std::cout << str << "\n";
+    std::cout << "\n";
+}
+
 static int count_adjacent_occupied(std::vector<std::string>& seats, int row, int col)
 {
     int rows = seats.size();
@@ -39,13 +47,6 @@ static int count_adjacent_occupied(std::vector<std::string>& seats, int row, int
     return n;
 }
 
-static void print_seats(const std::vector<std::string>& seats)
-{
-    for (auto str: seats)
-        std::cout << str << "\n";
-    std::cout << "\n";
-}
-
 int solution_for_puzzle_1(std::vector<std::string> seats)
 {
     std::vector<std::string> seats2 = seats;
@@ -56,7 +57,7 @@ int solution_for_puzzle_1(std::vector<std::string> seats)
 
     do { 
         changed = false;
-        print_seats(seats);
+        //print_seats(seats);
 
         for (int i = 0; i < rows; i++)
             for (int j = 0; j < cols; j++) {
@@ -65,6 +66,136 @@ int solution_for_puzzle_1(std::vector<std::string> seats)
                     seats2[i][j] = '#';
                     changed = true;
                 } else if (seats[i][j] == '#' && n >= 4) {
+                    seats2[i][j] = 'L';
+                    changed = true;
+                } else
+                    seats2[i][j] = seats[i][j];
+            }
+
+        std::vector<std::string> temp = std::move(seats);
+        seats = std::move(seats2);
+        seats2 = std::move(temp);
+    } while (changed);
+
+    int occupied = 0;
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            if (seats[i][j] == '#')
+                occupied++;
+
+    return occupied;
+}
+
+static int count_adjacent_occupied2(std::vector<std::string>& seats, int row, int col)
+{
+    int rows = seats.size();
+    int cols = seats[0].size();
+    int n = 0;
+    int i, j;
+
+    i = row - 1; j = col - 1;
+    while (i >= 0 && j >= 0) {
+        if (seats[i][j] == '#') {
+            n++;
+            break;
+        } else if (seats[i][j] == 'L')
+            break;
+        i--; j--;
+    }
+
+    i = row - 1; j = col;
+    while (i >= 0) {
+        if (seats[i][j] == '#') {
+            n++;
+            break;
+        } else if (seats[i][j] == 'L')
+            break;
+        i--;
+    }
+
+    i = row - 1; j = col + 1;
+    while (i >= 0 && j < cols) {
+        if (seats[i][j] == '#') {
+            n++;
+            break;
+        } else if (seats[i][j] == 'L')
+            break;
+        i--; j++;
+    }
+
+    i = row; j = col - 1;
+    while (j >= 0) {
+        if (seats[i][j] == '#') {
+            n++;
+            break;
+        } else if (seats[i][j] == 'L')
+            break;
+        j--;
+    }
+
+    i = row; j = col + 1;
+    while (j < cols) {
+        if (seats[i][j] == '#') {
+            n++;
+            break;
+        } else if (seats[i][j] == 'L')
+            break;
+        j++;
+    }
+
+    i = row + 1; j = col - 1;
+    while (i < rows && j >= 0) {
+        if (seats[i][j] == '#') {
+            n++;
+            break;
+        } else if (seats[i][j] == 'L')
+            break;
+        i++; j--;
+    }
+
+    i = row + 1; j = col;
+    while (i < rows) {
+        if (seats[i][j] == '#') {
+            n++;
+            break;
+        } else if (seats[i][j] == 'L')
+            break;
+        i++;
+    }
+
+    i = row + 1; j = col + 1;
+    while (i < rows && j < cols) {
+        if (seats[i][j] == '#') {
+            n++;
+            break;
+        } else if (seats[i][j] == 'L')
+            break;
+        i++; j++;
+    }
+
+    return n;
+}
+
+
+int solution_for_puzzle_2(std::vector<std::string> seats)
+{
+    std::vector<std::string> seats2 = seats;
+    bool changed;
+
+    int rows = seats.size();
+    int cols = seats[0].size();
+
+    do { 
+        changed = false;
+        //print_seats(seats);
+
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++) {
+                int n = count_adjacent_occupied2(seats, i, j);
+                if (seats[i][j] == 'L' && n == 0) {
+                    seats2[i][j] = '#';
+                    changed = true;
+                } else if (seats[i][j] == '#' && n >= 5) {
                     seats2[i][j] = 'L';
                     changed = true;
                 } else
@@ -101,7 +232,12 @@ int main()
         seats.push_back(line);
     }
 
-    int occupied = solution_for_puzzle_1(seats);
+    int occupied;
+
+    occupied = solution_for_puzzle_1(seats);
+    std::cout << occupied << " seats is occupied\n";
+
+    occupied = solution_for_puzzle_2(seats);
     std::cout << occupied << " seats is occupied\n";
 
     return 0;
