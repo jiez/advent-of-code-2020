2298cd5b2ee30f8216cc2b0e51c3b389a9fd92bf
Jie Zhang <jzhang918@gmail.com>
2020-12-11 01:24:22 -0500

    refactor solutions for day 11

--- 11/seating.cc
+++ 11/seating.cc
@@ -14,7 +14,7 @@ static void print_seats(const std::vector<std::string>& seats)
     std::cout << "\n";
 }
 
-static int count_adjacent_occupied(std::vector<std::string>& seats, int row, int col)
+static int count_adjacent_occupied1(std::vector<std::string>& seats, int row, int col)
 {
     int rows = seats.size();
     int cols = seats[0].size();
@@ -45,45 +45,6 @@ static int count_adjacent_occupied(std::vector<std::string>& seats, int row, int
         n++;
 
     return n;
-}
-
-int solution_for_puzzle_1(std::vector<std::string> seats)
-{
-    std::vector<std::string> seats2 = seats;
-    bool changed;
-
-    int rows = seats.size();
-    int cols = seats[0].size();
-
-    do { 
-        changed = false;
-        //print_seats(seats);
-
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < cols; j++) {
-                int n = count_adjacent_occupied(seats, i, j);
-                if (seats[i][j] == 'L' && n == 0) {
-                    seats2[i][j] = '#';
-                    changed = true;
-                } else if (seats[i][j] == '#' && n >= 4) {
-                    seats2[i][j] = 'L';
-                    changed = true;
-                } else
-                    seats2[i][j] = seats[i][j];
-            }
-
-        std::vector<std::string> temp = std::move(seats);
-        seats = std::move(seats2);
-        seats2 = std::move(temp);
-    } while (changed);
-
-    int occupied = 0;
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < cols; j++)
-            if (seats[i][j] == '#')
-                occupied++;
-
-    return occupied;
 }
 
 static int count_adjacent_occupied2(std::vector<std::string>& seats, int row, int col)
@@ -176,8 +137,9 @@ static int count_adjacent_occupied2(std::vector<std::string>& seats, int row, in
     return n;
 }
 
+typedef int (*count_occupied_func_t)(std::vector<std::string>&, int /*row*/, int /*col*/);
 
-int solution_for_puzzle_2(std::vector<std::string> seats)
+static int solution_common(std::vector<std::string>& seats, count_occupied_func_t f, int tolerance)
 {
     std::vector<std::string> seats2 = seats;
     bool changed;
@@ -191,11 +153,11 @@ int solution_for_puzzle_2(std::vector<std::string> seats)
 
         for (int i = 0; i < rows; i++)
             for (int j = 0; j < cols; j++) {
-                int n = count_adjacent_occupied2(seats, i, j);
+                int n = f(seats, i, j);
                 if (seats[i][j] == 'L' && n == 0) {
                     seats2[i][j] = '#';
                     changed = true;
-                } else if (seats[i][j] == '#' && n >= 5) {
+                } else if (seats[i][j] == '#' && n >= tolerance) {
                     seats2[i][j] = 'L';
                     changed = true;
                 } else
@@ -214,6 +176,16 @@ int solution_for_puzzle_2(std::vector<std::string> seats)
                 occupied++;
 
     return occupied;
+}
+
+static int solution_for_puzzle_1(std::vector<std::string> seats)
+{
+    return solution_common(seats, count_adjacent_occupied1, 4);
+}
+
+static int solution_for_puzzle_2(std::vector<std::string> seats)
+{
+    return solution_common(seats, count_adjacent_occupied2, 5);
 }
 
 
@@ -236,9 +208,11 @@ int main()
 
     occupied = solution_for_puzzle_1(seats);
     std::cout << occupied << " seats is occupied\n";
+    assert(occupied == 2489);
 
     occupied = solution_for_puzzle_2(seats);
     std::cout << occupied << " seats is occupied\n";
+    assert(occupied == 2180);
 
     return 0;
 }
