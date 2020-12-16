c4bc32b03140bb3ecfe1d3e056c3595c8dccfea9
Jie Zhang <jzhang918@gmail.com>
2020-12-16 02:50:25 -0500

    solution for puzzle 2 of day 16

--- 16/ticket.cc
+++ 16/ticket.cc
@@ -14,6 +14,15 @@ struct range_t {
     int max;
 };
 
+static bool field_valid_for_ranges(const std::vector<range_t>& ranges, int field)
+{
+    for (auto range: ranges)
+        if (field >= range.min && field <= range.max)
+            return true;
+
+    return false;
+}
+
 static bool field_valid(const std::vector<std::vector<range_t>>& valid_ranges, int field)
 {
     for (auto ranges: valid_ranges)
@@ -36,6 +45,95 @@ static int solution_for_puzzle_1(const std::vector<std::vector<range_t>>& valid_
     return error_rate;
 }
 
+static unsigned long long solution_for_puzzle_2(
+    const std::vector<std::string>& field_names,
+    const std::vector<std::vector<range_t>>& valid_ranges,
+    const std::vector<int>& my_ticket,
+    const std::vector<std::vector<int>>& nearby_tickets)
+{
+    std::vector<std::vector<int>> valid_tickets;
+    std::map<int, int> field_map;
+
+    for (auto ticket: nearby_tickets) {
+        bool is_valid = true;
+        for (auto field: ticket)
+            if (!field_valid(valid_ranges, field)) {
+                is_valid = false;
+                break;
+            }
+
+        if (is_valid)
+            valid_tickets.push_back(ticket);
+    }
+
+    // possible fields in tickets for each field name
+    std::vector<std::vector<int>> possible_fields;
+    int num_fields = valid_ranges.size();
+
+    for (int i = 0; i < num_fields; i++) {
+        std::vector<range_t> ranges = valid_ranges[i];
+        std::vector<int> fields;
+
+        for (int j = 0; j < num_fields; j++) {
+            bool is_valid = true;
+            for (auto ticket: valid_tickets) {
+                if (!field_valid_for_ranges(ranges, ticket[j])) {
+                    is_valid = false;
+                    break;
+                }
+            }
+
+            if (is_valid)
+                fields.push_back(j);
+        }
+
+        assert(fields.size() > 0);
+
+        possible_fields.push_back(fields);
+    }
+
+    /*
+    for (int i = 0; i < num_fields; i++) {
+        std::cout << i << ":";
+        for (auto field: possible_fields[i])
+            std::cout << " " << field;
+        std::cout << "\n";
+    }
+    */
+
+    while (true) {
+        int fixed;
+        int i;
+        for (i = 0; i < possible_fields.size(); i++)
+            if (possible_fields[i].size() == 1) {
+                fixed = possible_fields[i][0];
+                field_map[i] = fixed;
+                break;
+            }
+
+        if (i == possible_fields.size())
+            break;
+
+        for (i = 0; i < possible_fields.size(); i++) {
+            auto found = std::find(possible_fields[i].begin(), possible_fields[i].end(), fixed);
+            if (found != possible_fields[i].end())
+                possible_fields[i].erase(found);
+        }
+    }
+
+    /*
+    for (int i = 0; i < num_fields; i++)
+        std::cout << i << ":" << field_map[i] << "\n";
+    */
+
+    unsigned long long result = 1;
+    for (int i = 0; i < num_fields; i++)
+        if (field_names[i].substr(0, 9) == "departure")
+            result *= my_ticket[field_map[i]];
+        
+    return result;
+}
+
 
 int main()
 {
@@ -46,11 +144,12 @@ int main()
         return -1;
     }
 
+    std::vector<std::string> field_names;
     std::vector<std::vector<range_t>> valid_ranges;
     std::vector<int> my_ticket;
     std::vector<std::vector<int>> nearby_tickets;
 
-    std::regex valid_regex{"[a-z]*: ([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+)"};
+    std::regex valid_regex{"([a-z ]*): ([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+)"};
 
     std::string line;
     while (getline(input_file, line)) {
@@ -63,10 +162,11 @@ int main()
 
         std::smatch m;
         std::regex_search(line, m, valid_regex);
-        int min1 = stoi(m[1]);        
-        int max1 = stoi(m[2]);        
-        int min2 = stoi(m[3]);        
-        int max2 = stoi(m[4]);        
+        std::string field_name = m[1];
+        int min1 = stoi(m[2]);        
+        int max1 = stoi(m[3]);        
+        int min2 = stoi(m[4]);        
+        int max2 = stoi(m[5]);        
 
         range_t r1, r2;
         r1.min = min1;
@@ -78,6 +178,8 @@ int main()
         ranges.push_back(r2);
 
         valid_ranges.push_back(ranges);
+
+        field_names.push_back(field_name);
     }
 
     {
@@ -108,5 +210,7 @@ int main()
     error_rate = solution_for_puzzle_1(valid_ranges, nearby_tickets);
     std::cout << "error rate is " << error_rate << "\n";
 
+    unsigned long long result = solution_for_puzzle_2(field_names, valid_ranges, my_ticket, nearby_tickets);
+    std::cout << "result is " << result << "\n";
     return 0;
 }
