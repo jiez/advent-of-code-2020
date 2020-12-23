cc02f8525bb174ccf0e45985928918d5db0208f7
Jie Zhang <jzhang918@gmail.com>
2020-12-23 12:09:58 -0500

    a much faster solution for puzzle 2 of day 23 using circular list

--- 23/crab-cups.cc
+++ 23/crab-cups.cc
@@ -1,48 +1,129 @@
 #include <cassert>
 #include <string>
 #include <iostream>
-#include <algorithm>
-#include <deque>
-#include <iterator>
+#include <unordered_map>
 
-// we keep the current cup to be the first one
-static void one_move(std::deque<int>& circle)
-{
-    auto s = circle.begin() + 1;
-    auto t = circle.begin() + 4;
-    std::deque<int> three_cups(s, t);
+// NEXT points to the clockwise next cup
+// PREV points to the counter-clockwise next cup
+struct cup_t {
+    int label;
+    cup_t *prev;
+    cup_t *next;
+};
 
-    // remove the three cups
-    circle.erase(s, t);
+struct cup_circle_t {
+    int max;
+    int min;
+    cup_t *current;
+    std::unordered_map<int, cup_t *> lookup_table;
 
-    int current_cup = circle[0];
-    int dest_cup = current_cup;
-    do {
-        dest_cup--;
-        if (dest_cup < 1)
-            dest_cup = circle.size() + 3;
-    } while (std::find(three_cups.begin(), three_cups.end(), dest_cup) != three_cups.end());
+    cup_circle_t(): max{-1}, min{-1}, current{nullptr} {}
+    ~cup_circle_t() {
+        if (current == nullptr)
+            return;
 
-    auto dest = std::find(circle.begin(), circle.end(), dest_cup);
+        cup_t *p = current;
+        do {
+            cup_t *q = p->next;
+            delete p;
+            p = q;
+        } while (p != current);
 
-    circle.insert(dest + 1, three_cups.begin(), three_cups.end());
+        current = nullptr;
+    }
 
-    // move the next current cup to the first place
-    int first = circle.front();
-    circle.pop_front();
-    circle.push_back(first);
-}
+    // add cup N before CURRENT
+    void add(int n) {
+        cup_t *t = new cup_t;
+        t->label = n;
+        if (current == nullptr) {
+            t->prev = t;
+            t->next = t;
+            current = t;
+        } else {
+            cup_t *p = current->prev;
+            p->next = t;
+            t->prev = p;
+            t->next = current;
+            current->prev = t;
+        }
+        assert(lookup_table.find(n) == lookup_table.end());
+        lookup_table[n] = t;
+        if (min == -1 || n < min)
+            min = n;
+        if (max == -1 || n > max)
+            max = n;
+    }
 
-static void solution_for_puzzle_1(std::deque<int>& circle)
+    bool find(cup_t *from, size_t len, int cup) {
+        cup_t *p = from;
+        for (int i = 0; i < len; i++) {
+            if (p->label == cup)
+                return true;
+            p = p->next;
+        }
+        return false;
+    }
+
+    void one_move() {
+        int current_cup = current->label;
+        int dest_cup = current_cup;
+        do {
+            dest_cup--;
+            if (dest_cup < min)
+                dest_cup = max;
+        } while (find(current->next, 3, dest_cup));
+        cup_t *dest = lookup_table.find(dest_cup)->second;
+        cup_t *three_cups_first = current->next;
+        cup_t *three_cups_last = three_cups_first->next->next;
+
+        // take out 3 cups
+        current->next = three_cups_last->next;
+        three_cups_last->next->prev = current;
+
+        // insert 3 cups after dest
+        three_cups_last->next = dest->next;
+        dest->next->prev = three_cups_last;
+        dest->next = three_cups_first;
+        three_cups_first->prev = dest;
+
+        // move current to the next
+        current = current->next;
+    }
+
+    void set_current(int cup) {
+        assert(cup >= min && cup <= max);
+        current = lookup_table[cup];
+    }
+
+    void move_current_next() {
+        current = current->next;
+    }
+
+    int get_current_cup() {
+        return current->label;
+    }
+
+    // print cups from current cup, which is excluded
+    void print_cups() {
+        cup_t *p = current->next;
+        while (p != current) {
+            std::cout << p->label;
+            p = p->next;
+        }
+    }
+};
+
+static void solution_for_puzzle_1(cup_circle_t* circle)
 {
     for (int i = 0; i < 100; i++)
-        one_move(circle);
+        circle->one_move();
 }
 
-static void solution_for_puzzle_2(std::deque<int>& circle)
+static void solution_for_puzzle_2(cup_circle_t* circle)
 {
     for (int i = 0; i < 10000000; i++)
-        one_move(circle);
+        circle->one_move();
 }
 
 int main()
@@ -51,44 +132,33 @@ int main()
     // below is the test in the puzzle
     //const std::string input{"389125467"};
 
-    std::deque<int> circle;
+    cup_circle_t circle;
     for (auto c: input)
-        circle.push_back(c - '0');
+        circle.add(c - '0');
 
-    solution_for_puzzle_1(circle);
+    solution_for_puzzle_1(&circle);
 
-    auto idx = std::find(circle.begin(), circle.end(), 1);
+    circle.set_current(1);
     std::cout << "result is ";
-    for (auto it = std::next(idx); it != circle.end(); ++it)
-        std::cout << *it;
-    for (auto it = circle.begin(); it != idx; ++it)
-        std::cout << *it;
+    circle.print_cups();
     std::cout << "\n";
 
     // part 2
     unsigned long long result;
 
-    std::deque<int> circle2;
+    cup_circle_t circle2;
     for (auto c: input)
-        circle2.push_back(c - '0');
+        circle2.add(c - '0');
     for (int i = 10; i <= 1000000; i++)
-        circle2.push_back(i);
+        circle2.add(i);
 
-    solution_for_puzzle_2(circle2);
+    solution_for_puzzle_2(&circle2);
 
-    auto idx2 = std::find(circle2.begin(), circle2.end(), 1);
-
-    idx2 = std::next(idx2);
-    if (idx2 == circle2.end())
-        idx2 = circle2.begin();
-    std::cout << "first cup: " << *idx2 << "\n";
-    result = *idx2;
-
-    idx2 = std::next(idx2);
-    if (idx2 == circle2.end())
-        idx2 = circle2.begin();
-    std::cout << "second cup: " << *idx2 << "\n";
-    result *= *idx2;
+    circle2.set_current(1);
+    circle2.move_current_next();
+    result = circle2.get_current_cup();
+    circle2.move_current_next();
+    result *= circle2.get_current_cup();
 
     std::cout << "result is " << result << "\n";
 
