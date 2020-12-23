9012525d86b9071d54cccba05cec7959c8594663
Jie Zhang <jzhang918@gmail.com>
2020-12-23 02:31:40 -0500

    brutal force solution for puzzle 2 of day 23

--- 23/crab-cups.cc
+++ 23/crab-cups.cc
@@ -20,7 +20,7 @@ static void one_move(std::deque<int>& circle)
     do {
         dest_cup--;
         if (dest_cup < 1)
-            dest_cup = 9;
+            dest_cup = circle.size() + 3;
     } while (std::find(three_cups.begin(), three_cups.end(), dest_cup) != three_cups.end());
 
     auto dest = std::find(circle.begin(), circle.end(), dest_cup);
@@ -36,6 +36,12 @@ static void one_move(std::deque<int>& circle)
 static void solution_for_puzzle_1(std::deque<int>& circle)
 {
     for (int i = 0; i < 100; i++)
+        one_move(circle);
+}
+
+static void solution_for_puzzle_2(std::deque<int>& circle)
+{
+    for (int i = 0; i < 10000000; i++)
         one_move(circle);
 }
 
@@ -58,6 +64,33 @@ int main()
     for (auto it = circle.begin(); it != idx; ++it)
         std::cout << *it;
     std::cout << "\n";
+
+    // part 2
+    unsigned long long result;
+
+    std::deque<int> circle2;
+    for (auto c: input)
+        circle2.push_back(c - '0');
+    for (int i = 10; i <= 1000000; i++)
+        circle2.push_back(i);
+
+    solution_for_puzzle_2(circle2);
+
+    auto idx2 = std::find(circle2.begin(), circle2.end(), 1);
+
+    idx2 = std::next(idx2);
+    if (idx2 == circle2.end())
+        idx2 = circle2.begin();
+    std::cout << "first cup: " << *idx2 << "\n";
+    result = *idx2;
+
+    idx2 = std::next(idx2);
+    if (idx2 == circle2.end())
+        idx2 = circle2.begin();
+    std::cout << "second cup: " << *idx2 << "\n";
+    result *= *idx2;
+
+    std::cout << "result is " << result << "\n";
 
     return 0;
 }
